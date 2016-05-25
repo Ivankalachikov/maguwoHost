31effa8fb65be7ab490d4eee1232604eb7ff2689
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2016-05-25 15:20:10 +0700

    add responsive style

---
+++ app/js/menu.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+	var icon = document.querySelector('.page-nav__icon');
+	var menu = document.querySelector('.page-nav__list');
+	resize();
+
+	icon.addEventListener('click', function () {
+		menu.classList.toggle('hidden');
+	});
+
+	document.addEventListener('click', function (event){
+		if (event.target.closest('.page-nav__icon') == null){
+			menu.classList.add('hidden');
+		}
+	});
+
+	function resize() { 
+    var ww = window.innerWidth;
+    if (ww>768){
+    	menu.classList.remove('hidden');
+    	icon.classList.add('hidden');
+    }
+    else{
+    	menu.classList.add('hidden');
+    	icon.classList.remove('hidden');
+    }
+  }
+
+  window.onresize = resize;
+
+});
